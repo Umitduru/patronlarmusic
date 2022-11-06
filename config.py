4e0e52e4a58ab89a78e8dda278902a7581e329c7
Umitduru <91970384+Umitduru@users.noreply.github.com>
2022-11-06 03:57:18 +0300

    patronlarmusic

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "tg_yenimuzik") 
+BOT_NAME = getenv("BOT_NAME", "patronlarsohbet_tr") 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "tg_yenimuzik")
