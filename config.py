0942e5338be3b4352c771ec4d8ffe36742660216
Umitbey <91970384+Umitduru@users.noreply.github.com>
2024-05-19 22:37:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ que = {}
 admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "patronlarsohbet_tr") 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "tg_yenimuzik")
+BOT_TOKEN = getenv("")
+BOT_NAME = getenv("BOT_NAME", "BusraMuzikBot") 
+API_ID = int(getenv("API_ID")19757862)
+API_HASH = getenv("7131164622:AAGLsJLFilmVEGfslTMoNIHAErIQPryjJow")
+BOT_USERNAME = getenv("BOT_USERNAME", "BusraMuzik_bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split(6790545367)))
